1b606eff5c9c7266b521a2b1ff87c1f52b64d82d
Matthew McGuff <matthew.mcguff@walmart.com>
2018-08-22 09:26:42 -0500

    add scss transpiling inline

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,18 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'app.bundle.js'
   },
+  module:{
+    rules:[
+      {test:/\.scss$/, use: ['style-loader','css-loader', 'sass-loader']}
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Project Template | freecodecamp.org',
       minify:{
           collapseWhitespace: true
       },
+      hash: true,
       template: './src/index.html'
     })
   ]
